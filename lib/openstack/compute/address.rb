8ddfdd001cc503eeac277f1626e7d3f7188b2702
marios <marios@redhat.com>
2012-07-03 14:19:54 +0300

    Fixes labelling of IP Addresses - public vs private
    Uses pattern matching against 10.0.0.0/8 192.168.0.0/16,
    172.16.0.0/12.
    
    Relevant/background:
    http://www.mail-archive.com/openstack@lists.launchpad.net/msg12987.html
    https://answers.launchpad.net/nova/+question/185110

--- lib/openstack/compute/address.rb
+++ lib/openstack/compute/address.rb
@@ -1,3 +1,4 @@
+require 'ipaddr'
 module OpenStack
 module Compute
 
@@ -33,6 +34,34 @@ module Compute
         @version = version
       end
     end
+
+    NON_ROUTABLE_ADDRESSES = [IPAddr.new("10.0.0.0/8"), IPAddr.new("192.168.0.0/16"), IPAddr.new("172.16.0.0/12")]
+
+    def self.is_private?(address_string)
+      NON_ROUTABLE_ADDRESSES.each do |no_route|
+        return true if no_route.include?(address_string)
+      end
+      false
+    end
+
+    #IN:  { "private"=> [{"addr"=>"10.7.206.171", "version"=>4}, {"addr"=>"15.185.160.208", "version"=>4}]}
+    #OUT: { "private"=> [{"addr"=>"10.7.206.171", "version"=>4}],
+    #       "public"=>  [{"addr"=>"15.185.160.208", "version"=>4}] }
+    def self.fix_labels(addresses_info)
+      addresses_info.inject({"public"=>[], "private"=>[]}) do |res, (label,address_struct_list)|
+        address_struct_list.each do |address_struct|
+          if(address_struct["version"==6])#v6 addresses are all routable...
+            res["public"] << address_struct
+          else
+            is_private?(address_struct["addr"])? res["private"] << address_struct : res["public"] << address_struct
+          end
+        end
+        res
+      end
+    end
+
+
+
   end
 
 end
