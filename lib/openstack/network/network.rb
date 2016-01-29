922fc6586bf2b9db9b8398b4d35ccdb25c038556
Aleksandr Salkov <asalkov@at-consulting.ru>
2016-01-30 01:59:21 +0300

    Add attribute 'external' to Network

--- lib/openstack/network/network.rb
+++ lib/openstack/network/network.rb
@@ -1,26 +1,25 @@
 module OpenStack
-module Network
-  class Network
+  module Network
+    class Network
+      attr_reader :id
+      attr_reader :name
+      attr_reader :admin_state_up
+      attr_reader :status
+      attr_reader :subnets
+      attr_reader :shared
+      attr_reader :external
+      attr_reader :tenant_id
 
-  attr_reader :id
-  attr_reader :name
-  attr_reader :admin_state_up
-  attr_reader :status
-  attr_reader :subnets
-  attr_reader :shared
-  attr_reader :tenant_id
-
-  def initialize(net_info={})
-    @id = net_info["id"]
-    @name = net_info["name"]
-    @admin_state_up = net_info["admin_state_up"]
-    @status = net_info["status"]
-    @subnets = net_info["subnets"]
-    @shared = net_info["shared"]
-    @tenant_id = net_info["tenant_id"]
-  end
-
+      def initialize(net_info = {})
+        @id = net_info['id']
+        @name = net_info['name']
+        @admin_state_up = net_info['admin_state_up']
+        @status = net_info['status']
+        @subnets = net_info['subnets']
+        @shared = net_info['shared']
+        @external = net_info['router:external']
+        @tenant_id = net_info['tenant_id']
+      end
+    end
   end
 end
-end
-
